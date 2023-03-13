fab692ee901c38dad7e5434f9e2f6951527ce6f0
gsm-abdullahi <onikokoabdullahi@gmail.com>
2023-03-13 22:16:26 +0100

    Added mirage js

---
+++ src/server.js
@@ -0,0 +1,25 @@
+import { createServer, Model } from "miragejs";
+
+export function makeServer({ environment = "development" } = {}) {
+  let server = createServer({
+    environment,
+
+    models: {
+      user: Model,
+    },
+
+    seeds(server) {
+      server.create("user", { name: "Bob" });
+    },
+
+    routes() {
+      this.namespace = "api";
+
+      this.get("/users", (schema) => {
+        return schema.users.all();
+      });
+    },
+  });
+
+  return server;
+}

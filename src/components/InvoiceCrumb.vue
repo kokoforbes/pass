230eaa4bcb8258b96709e99bb65081ef234534b1
gsm-abdullahi <onikokoabdullahi@gmail.com>
2023-03-15 00:31:56 +0100

    Created InvoiceCrumb component

---
+++ src/components/InvoiceCrumb.vue
@@ -0,0 +1,42 @@
+<template>
+  <nav class="invoice-crumb" aria-label="breadcrumbs">
+    <router-link to="/invoices" class="text-primary text-bold">
+      Invoices
+    </router-link>
+    <span>/</span>
+    <span
+      >edit invoice
+      <span class="text-bold">({{ invoiceId.toUpperCase() }})</span>
+    </span>
+  </nav>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "InvoiceCrumb",
+  props: {
+    invoiceId: {
+      type: String,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.invoice-crumb {
+  display: flex;
+  align-items: center;
+  gap: 0.25rem;
+
+  a {
+    font-size: 0.85rem;
+  }
+
+  span {
+    color: $secondary;
+  }
+}
+</style>

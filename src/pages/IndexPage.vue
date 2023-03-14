32553ca2b3d4c39763d033d680496dd0becae2fb
gsm-abdullahi <onikokoabdullahi@gmail.com>
2023-03-14 08:39:17 +0100

    Created activity, dashboard, help, wallet pages

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,11 +1,15 @@
 <template>
-  <q-page class="flex flex-center"> </q-page>
+  <q-page class="flex flex-center">
+    <invoice-card />
+  </q-page>
 </template>
 
 <script>
+import InvoiceCard from "src/components/InvoiceCard.vue";
 import { defineComponent } from "vue";
 
 export default defineComponent({
+  components: { InvoiceCard },
   name: "IndexPage",
 });
 </script>

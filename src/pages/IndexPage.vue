2122c7cd652336bcde9980c8577278330dfac299
gsm-abdullahi <onikokoabdullahi@gmail.com>
2023-03-16 13:49:37 +0100

    Updated IndexPage

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,51 +1,66 @@
 <template>
   <div class="invoice-page container-padding">
     <invoice-crumb :invoice-id="'inv-2022-010'" />
-    <invoice-card />
+    <div v-if="isLoading" class="loader">
+      <q-circular-progress
+        indeterminate
+        size="5rem"
+        color="primary"
+        class="q-ma-md"
+      />
+    </div>
+    <invoice-card v-else :invoice="data" />
   </div>
 </template>
 
 <script>
 import InvoiceCard from "src/components/Invoice/InvoiceCard.vue";
 import InvoiceCrumb from "src/components/Invoice/InvoiceCrumb.vue";
-import { defineComponent, onMounted, ref } from "vue";
 
-export default defineComponent({
+export default {
   components: { InvoiceCard, InvoiceCrumb },
   name: "IndexPage",
 
-  setup() {
-    const isLoading = ref(false);
-    const invoiceId = ref("inv-2022-010");
-    onMounted(() => {
-      if (process.env.NODE_ENV === "development") {
-        getInvoice();
-      }
-    });
-
-    const getInvoice = async () => {
-      try {
-        isLoading.value = true;
-        const response = await fetch(`api/invoice/${invoiceId.value}`);
-        console.log(response);
-        // response.json().then((data) => {
-        //   console.log(data);
-        // });
-        isLoading.value = false;
-      } catch (error) {
-        console.log(error);
-        isLoading.value = false;
-      }
+  data() {
+    return {
+      invoiceId: "inv-2022-010",
+      data: null,
+      isLoading: true,
     };
   },
-});
+
+  async created() {
+    this.isLoading = true;
+    await fetch("/api/invoice")
+      .then((res) => res.json())
+      .then((json) => {
+        this.data = json.users[0];
+        console.log(this.data);
+      });
+    this.isLoading = false;
+  },
+};
 </script>
 
 <style lang="scss" scoped>
 .invoice-page {
-  padding: 2rem 1rem 2rem 0;
+  padding: 20px 20px 40px;
   max-width: $max-width;
   gap: 3rem;
   width: 100%;
+
+  .loader {
+    min-width: 100%;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+}
+
+@media (min-width: $breakpoint--md) {
+  .invoice-page {
+    padding: 20px 26px 40px;
+  }
 }
 </style>
